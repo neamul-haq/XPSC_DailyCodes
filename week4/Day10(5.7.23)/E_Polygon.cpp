05cf821e430ace11f7739912b4a09481f6cd5cdd
Neamul <neamul.cse6.bu@gmail.com>
2023-07-05 23:08:34 +0600

    Week4 Day10 updates

---
+++ week4/Day10(5.7.23)/E_Polygon.cpp
@@ -0,0 +1,30 @@
+/*
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define ndl '\n';
+#define cyes cout << "YES" << '\n';
+#define cno cout << "NO" << '\n';
+#define print cout << ans << '\n';
+#define ump unordered_map<int,int>mp;
+#define mp map<int,int>mp;
+int const mod = 1e9+7;
+const ll inf = 1e18;
+const ll N = 1e3 ;
+#define   fast() {ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
+#define all(x) (x).begin(), (x).end()
+void solve();
+
+int main()
+{
+    fast()
+    ll t=1; cin >> t;
+    while(t--) solve();
+}
+
+void solve(){
+    ll n,ans=0; cin >> n;
+
+
+}
