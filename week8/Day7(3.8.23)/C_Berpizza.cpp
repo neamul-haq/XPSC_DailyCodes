e7fc094e94e2746f764565114d3eb467a7d1a3bb
Neamul <neamul.cse6.bu@gmail.com>
2023-08-05 13:23:48 +0600

    Week8 Day7 updated

--- week8/Day7(3.8.23)/C_Berpizza.cpp
+++ week8/Day7(3.8.23)/C_Berpizza.cpp
@@ -20,16 +20,52 @@ const ll N = 1e3 ;
 #define   fast() {ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
 #define all(x) (x).begin(), (x).end()
 void solve();
+#define pp pair<int, int>
 
 int main()
 {
     fast()
-    ll t=1; cin >> t;
+    ll t=1;
     while(t--) solve();
 }
 
 void solve(){
-    int a,b,c;
+    int n;
+    cin >> n;
+    int idx=1;
     map<int,int>mono;
-    map<int,int,greater<int>>poly;
+    multimap<int,int,greater<int>>poly;
+    priority_queue <pp, vector<pp>, greater<pp> > pq;
+    vector<int>ans;
+    while(n--){
+        int type;
+        cin >> type;
+        auto it3=poly.begin();
+        if(type==1){
+            int val;
+            cin >> val;
+            mono.insert({idx,val});
+            poly.insert({val,idx});
+            //pq.push({})
+            idx++;
+        }
+        else if(type==2){
+            for(auto it:mono){
+                    ans.push_back(it.first);
+                    poly.erase(poly.find(it.second));
+                    mono.erase(it.first);
+                    break;
+            }
+        }
+        else{
+            ans.push_back(it3->second);
+            mono.erase(it3->second);
+            poly.erase(it3);
+        }
+    }
+    for(auto u:ans)
+    {
+        cout << u << " ";
+    }
+    cout << ndl;
 }
