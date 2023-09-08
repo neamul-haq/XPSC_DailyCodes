80b8f9b5fe9882286b6f6e981c3f607f9bb2e826
Neamul <neamul.cse6.bu@gmail.com>
2023-09-08 23:34:46 +0600

    Week14 Day1 updated

---
+++ week14/Day1(08.09.23)/B_The_Corridor_or_There_and_Back_Again.cpp
@@ -0,0 +1,52 @@
+/*
+AUTHOR: Neyamul_Haq
+CREATED: 08-09-2023  11:36:28
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef long long int ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+#define ndl '\n';
+#define cyes cout << "YES" << '\n';
+#define cno cout << "NO" << '\n';
+#define print cout << ans << '\n';
+#define f(i,n) for(int i=0; i<n; i++)
+#define ump unordered_map<int,int>um;
+#define mp map<int,int>m;
+#define sz(a) (int)(a).size()
+int const mod = 1e9+7;
+const ll inf = 1e18;
+const ll N = 1e3 ;
+#define   fast() {ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
+#define all(x) (x).begin(), (x).end()
+void solve();
+
+int main()
+{
+#ifndef LOKAL
+    fast()
+#endif
+    ll t=1; cin >> t;
+    while(t--) solve();
+}
+
+void solve(){
+    int n,ans=INT_MAX; cin >> n;
+    while(n--){
+        int x,y;
+        cin >> x >> y;
+        ans=min(ans,x+((y+1)/2)-1);
+    }
+    print
+}
